b3309b7c2283d2d97739b60a017371bece91a30d
Jefte <jefte.justino@gmail.com>
2019-11-22 22:47:31 -0300

    Feature: Dashboard with Check-ins

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,100 @@
-import React from 'react';
-import { Text } from 'react-native';
-
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import Button from '~/components/Button';
+
+import {
+  Container,
+  CheckinList,
+  CheckinItem,
+  CheckinNumber,
+  CheckinDate,
+} from './styles';
+
+import api from '~/services/api';
 
 export default function Dashboard() {
-  return <Text>Dashboard</Text>;
+  const [checkins, setCheckins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const userId = useSelector(state => state.user.profile.id);
+
+  useEffect(() => {
+    async function getCheckins() {
+      try {
+        const response = await api.get(`students/${userId}/checkin`);
+
+        let total = response.data.length;
+        const data = response.data.map(item => ({
+          ...item,
+          order: total--,
+          dateFormatted: formatRelative(parseISO(item.createdAt), new Date(), {
+            locale: pt,
+            addSuffix: true,
+          }),
+        }));
+
+        setCheckins(data);
+      } catch (error) {
+        console.tron.log(error);
+      }
+    }
+
+    getCheckins();
+  }, [userId]);
+
+  async function handleCheckin() {
+    setLoading(true);
+    try {
+      const response = await api.post(`students/${userId}/checkin`);
+
+      const total = checkins[0] ? checkins[0].order + 1 : 1;
+      const data = {
+        ...response.data,
+        order: total,
+        dateFormatted: formatRelative(
+          parseISO(response.data.createdAt),
+          new Date(),
+          {
+            locale: pt,
+            addSuffix: true,
+          }
+        ),
+      };
+
+      setCheckins([data, ...checkins]);
+    } catch (error) {
+      Alert.alert(
+        'Falha ao fazer check-in',
+        'Você não pode mais realizar check-ins por enquanto!'
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <Container>
+      <CheckinList
+        ListHeaderComponent={
+          <Button loading={loading} onPress={() => handleCheckin()}>
+            Novo check-in
+          </Button>
+        }
+        data={checkins}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <CheckinItem>
+            <CheckinNumber>Check-in # {item.order}</CheckinNumber>
+            <CheckinDate>{item.dateFormatted}</CheckinDate>
+          </CheckinItem>
+        )}
+      />
+    </Container>
+  );
 }
 
 Dashboard.navigationOptions = {
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const CheckinList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 },
+})`
+  margin-top: 10px;
+`;
+
+export const CheckinItem = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+  border: 1px solid #ddd;
+  height: 46px;
+  padding: 0px 15px;
+  border-radius: 4px;
+  margin-top: 15px;
+`;
+
+export const CheckinNumber = styled.Text`
+  color: #444;
+  font-weight: bold;
+  font-size: 14px;
+`;
+
+export const CheckinDate = styled.Text`
+  color: #666;
+  font-size: 14px;
+`;
